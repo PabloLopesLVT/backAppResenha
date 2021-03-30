a9dcdbc0be331992c79fb39cf6610b6eae6fe5f7
whentony <whentony.ferreira@gmail.com>
2021-03-30 20:34:43 -0300

    Conserto da API

--- app/Http/Controllers/api/UsuarioApiController.php
+++ app/Http/Controllers/api/UsuarioApiController.php
@@ -3,31 +3,48 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Endereco;
 use Illuminate\Http\Request;
 use App\Models\Usuario;
+use Illuminate\Support\Facades\DB;
 
 class UsuarioApiController extends Controller
 {
-    public function store(Request $request){
-
+    public function store(Request $request)
+    {
+        $endereco = new Endereco();
         $usuario = new Usuario();
 
         if ($request->id == '') {
-         $this->validate($request, $usuario->rules());
+            $this->validate($request, $usuario->rules());
+            $endereco->logradouro = $request->input('logradouro');
+            $endereco->numero = $request->input('numero');
+            $endereco->bairro = $request->input('bairro');
+            $endereco->cep = $request->input('cep');
+            $endereco->municipio = $request->input('municipio');
+            $endereco->estado = $request->input('estado');
+            $endereco->complemento = $request->input('complemento');
+
+            $salvar  = $endereco->save();
+
+            if ($salvar) {
+            $endereco  = DB::table('enderecos')->orderBy('id', 'desc')->first();
 
             $usuario->nome = $request->input('nome');
             $usuario->sobrenome = $request->input('sobrenome');
             $usuario->email = $request->input('email');
             $usuario->cpf = $request->input('cpf');
             $usuario->tipo = $request->input('tipo');
-            $usuario->endereco_id = $request->input('endereco_id');
+            $usuario->endereco_id = $endereco->id;
+
             $salvar  = $usuario->save();
 
-            if($salvar){
-                $msg = "Registro salvo com sucesso!";
-                return response()->json(['message' => $msg], 201);
+                if ($salvar) {
+                    $msg = "Registro salvo com sucesso!";
+                    return response()->json(['message' => $msg], 201);
+                }
             }
-        }else{
+        } else {
 
             $request->validate([
                 'nome' => 'required',
@@ -36,21 +53,31 @@ class UsuarioApiController extends Controller
                 'cpf' => 'required|cpf',
                 'tipo' => 'required',
             ]);
+            $endereco  = Endereco::find($request->input('idendereco'));
+            $endereco->logradouro = $request->input('logradouro');
+            $endereco->numero = $request->input('numero');
+            $endereco->bairro = $request->input('bairro');
+            $endereco->cep = $request->input('cep');
+            $endereco->municipio = $request->input('municipio');
+            $endereco->estado = $request->input('estado');
+            $endereco->complemento = $request->input('complemento');
+
+            $salvar  = $endereco->save();
+
             $usuario  = Usuario::find($request->input('id'));
             $usuario->nome = $request->input('nome');
             $usuario->sobrenome = $request->input('sobrenome');
             $usuario->email = $request->input('email');
             $usuario->cpf = $request->input('cpf');
             $usuario->tipo = $request->input('tipo');
-            $usuario->endereco_id = $request->input('endereco_id');
+            $usuario->endereco_id = $request->input('idendereco');
 
             $update  = $usuario->update();
             $msg = "Atualização salva com sucesso!";
-            if($update){
+            if ($update) {
                 return response()->json(['message' => $msg], 201);
             }
         }
-
     }
 
     public function destroy($id)
@@ -58,24 +85,18 @@ class UsuarioApiController extends Controller
 
         $usuario = Usuario::find($id);
 
-        try{
+        try {
             $delete = $usuario->delete();
-
-        }catch (\Illuminate\Database\QueryException $e) {
+        } catch (\Illuminate\Database\QueryException $e) {
 
             //Tenho que ver pra onde vou mandar o cara quando ele deletar
             $msg = "Você não pode excluir o usuário $usuario->nome porque há registros em dependência desse registro.";
             return response()->json(['message' => $msg], 404);
-
         }
 
-        if($delete){
+        if ($delete) {
             $msg = "Deleção realizada com sucesso!";
             return response()->json(['message' => $msg], 201);
         }
-
-
-        return view('empresa.create',  ['msg' => $msg, 'status' => $status]);
     }
-
 }
